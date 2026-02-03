cde4de838d4f90da17df81c709103d1841c11716
Mukul <mukulkashyap.business@gmail.com>
2026-02-03 23:30:50 +0530

    feat: basic functions for addition, subtraction, multiplication and division

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(num1, num2){
+    return (num1 + num2);
+}
+
+function subtract(num1, num2){
+    return (num1 - num2);
+}
+
+function multiply(num1, num2){
+    return (num1 * num2);
+}
+
+function divide(num1, num2){
+    if(num2 == 0){
+        return "Division by zero is not possible";
+    }
+    else{
+        return (num1 / num2);
+    }
+}
+
