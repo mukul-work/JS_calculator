1755960d1e091f61ffa3d6a6a2aa36d821814b79
Mukul <mukulkashyap.business@gmail.com>
2026-02-04 23:51:45 +0530

    add: prototype 1

--- script.js
+++ script.js
@@ -1,94 +1,61 @@
+const parser = new exprEval.Parser();
+
 const display = document.querySelector("#display");
 const digits = document.querySelectorAll(".digit");
+const decimal = document.querySelector("#decimal");
 const clearBtn = document.querySelector("#clear");
 const equalBtn = document.querySelector("#equals");
 const operatorBtn = document.querySelectorAll(".operator");
 
-let num1 = "";
-let num2 = "";
 let operator = "";
-let currOp = null;
-let resetDisplay = false;
+let isOperator = false;
+let isDecimal = false;
+let resetDisplay = true;
+let expression = "";
 
-function updateDisplay(num){
-    display.textContent = num || "0";
+
+function updateDisplay(text){
+    display.textContent = text || "0";
 }
 
 function manageDigits(event){
     if(resetDisplay){
         display.textContent = "";
-        resetDisplay = false;
     }
-
+    expression += event.target.textContent;
     display.textContent += event.target.textContent;
+    isOperator = false;
+    resetDisplay = false;
 }
 
 function manageOperators(event){
-    if(currOp !== null) evaluate();
+    if(isOperator) return;
+    expression += event.target.textContent;
+    display.textContent += event.target.textContent;
+    isOperator = true;
+    isDecimal = false;
+}
 
-    num1 = display.textContent;
-    currOp = event.target.textContent;
-    resetDisplay = true;
+function manageDecimal(event){
+    if(isDecimal) return;
+    expression += event.target.textContent;
+    display.textContent += event.target.textContent;
+    isDecimal = true;
 }
 
 function evaluate(){
-    if(currOp === null) return;
-
-    num2 = display.textContent;
-
-    const result = operate(Number(num1), Number(num2), currOp);
-
+    let result = parser.evaluate(expression);
+    result = Math.round(result*1000)/1000;
     updateDisplay(result);
+    expression = String(result);
     currOp = null;
-    resetDisplay=true;
+
 
 }
 
 function clearCalc(){
-    num1 = "";
-    num2 = "";
     currOp = null;
     updateDisplay("0");
-}
-
-function add(num1, num2){
-    return (num1 + num2);
-}
-
-function subtract(num1, num2){
-    return (num1 - num2);
-}
-
-function multiply(num1, num2){
-    return (num1 * num2);
-}
-
-function divide(num1, num2){
-    if(num2 == 0){
-        return "Division by zero is not possible";
-    }
-    else{
-        return (num1 / num2);
-    }
-}
-
-
-function operate(num1, num2, operator){
-    if(operator == '+'){
-        return add(num1, num2);
-    }
-    else if(operator == '-'){
-        return subtract(num1, num2);
-    }
-    else if(operator == '*'){
-        return multiply(num1, num2);
-    }
-    else if(operator == '/'){
-        return divide(num1, num2);
-    }
-    else{
-        alert('Please choose a valid operator');
-    }
 }
 
 digits.forEach(btn => {
@@ -98,6 +65,8 @@ digits.forEach(btn => {
 operatorBtn.forEach(btn => {
     btn.addEventListener("click", manageOperators);
 })
+
+decimal.addEventListener("click", manageDecimal);
 
 equalBtn.addEventListener("click", evaluate);
 clearBtn.addEventListener("click", clearCalc);
